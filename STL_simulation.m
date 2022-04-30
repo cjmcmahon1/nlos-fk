4dabaeab1032950c8b56d2e528237319fca46513
Chris McMahon <cjm01@bu.edu>
2022-04-30 11:26:41 -0400

    add reflectances of object

--- STL_simulation.m
+++ STL_simulation.m
@@ -1,5 +1,16 @@
+%%
 data = stlread("3DBenchy.stl");
-obj_coords = downsample(data.Points, 1);
+obj_coords = downsample(data.Points, 10);
+%reorder object coordinates by z-height for reflectance
+% obj_coords = sort(obj_coords, 3); %not actually needed
+%organize reflectance such that objects at increasing z height have
+%increasing reflectance
+reflectances = zeros(size(obj_coords, 1), 1);
+reflectances(obj_coords(:,3) < 10) = 5;
+reflectances((obj_coords(:,3) < 30) & (obj_coords(:,3) >= 10)) = 7;
+reflectances((obj_coords(:,3) >= 30)) = 10;
+obj_coords = cat(2, obj_coords, reflectances);
+%generate coordinates of wall
 wall_res = 64;
 xvals = linspace(-120, 0, wall_res);
 yvals = ones(1, 32) .* 60;
@@ -10,26 +21,22 @@ trimesh(data, 'FaceColor', 'none', 'EdgeColor', 'k');
 xlabel('X'); ylabel('Y'); zlabel('Z');
 hold on;
 wall_coords = [X(:) 60.*ones(size(X(:), 1), 1) Z(:)];
-% scatter3(X(:), 60.*ones(size(X(:), 1), 1), Z(:), 'b');
 scatter3(wall_coords(:,1), wall_coords(:,2), wall_coords(:,3), 'b');
-diffx = obj_coords(1, 1) - xvals(16);
-diffy = obj_coords(1, 2) - yvals(16);
-diffz = obj_coords(1, 3) - zvals(16);
+%generate parallel lines coming from each wall coordinate
 lines = line(wall_coords, pi/4);
-linedist = distance_to_line(obj_coords(1,:), lines(:,:,1), lines(:,:,2));
 
-threshold = 1;
-timing_res = 256;
-max_wall_dist = 5;
+threshold = 3; %max distance between ray & object point to consider "hit"
+timing_res = 256; %number of bins in timing histogram
+max_ray_length = 5; %parameter to scale the ray length into a time 
 plotall_lines = false;
 time_coords = zeros(wall_res^2, timing_res);
 ray_distances = zeros(wall_res^2, 1);
-c = 1.5e8;
+c = 1.5e8; %speed of light halved to account for the doubled path
 for l = 1:size(lines, 1)
     %for each line, find all object points sufficiently close to that line
     %(within the above threshold). Then, pick the point with the shortest
     %euclidean distance to the wall as the reflection point. 
-    obj_distances = distance_to_line(obj_coords, ...
+    obj_distances = distance_to_line(obj_coords(:,1:3), ...
                                      lines(l, :, 1), lines(l, :, 2));
     obj_coords_on_line = obj_coords(obj_distances < threshold, :);
     %make a histogram of timing values, 1 if a hit was detected, 0 if no
@@ -37,15 +44,16 @@ for l = 1:size(lines, 1)
     timing_hist = zeros(timing_res, 1); 
     if size(obj_coords_on_line, 1) >= 1
         %plot the vector of the shortest distance ray that hits the object
-        vec_to_wall = obj_coords_on_line - lines(l, :, 1);
+        vec_to_wall = obj_coords_on_line(:, 1:3) - lines(l, :, 1);
         [min_val, min_idx] = min(vecnorm(vec_to_wall, 2, 2));
         %store the (x, y, t) measurement using the ray distance and the
         %speed of light
         end_coord = obj_coords_on_line(min_idx,:);
+        reflectance = end_coord(4);
         %set the hit to be at the closest histogram bin
-        rel_distance = (sqrt(min_val)-7) / max_wall_dist;
-        timing_hist(round(rel_distance * timing_res)) = 1;
-        pltline(lines(l, :, 1), end_coord, 0.8)
+        rel_distance = (sqrt(min_val)-7) / max_ray_length;
+        timing_hist(round(rel_distance * timing_res)) = reflectance;
+        pltline(lines(l, :, 1), end_coord(1:3), 0.8)
         ray_distances(l) = sqrt(min_val);
     elseif plotall_lines
         pltline(lines(l, :, 1), lines(l, :, 2), 0.1)
@@ -57,7 +65,7 @@ hold off;
 
 % run f-k migration
 wall_size = 2; % scanned area is 2 m x 2 m
-tof = ones(wall_res, wall_res).*1e-3;
+tof = ones(wall_res, wall_res); %tof is arbitrarily set to one in perfect sim
 fprintf('\nRunning f-k migration\n');
 fk = cnlos_reconstruction(meas_sim, tof, wall_size, 2, 256);
 
